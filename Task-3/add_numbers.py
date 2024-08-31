29a7358347d2f2b16fc22fcddcb89cbd68573d19
MUPPALLA SIVA RAMA KRISHNA <72961357+sivaramakrishna-27@users.noreply.github.com>
2024-08-31 21:21:15 +0530

    Added Task3

---
+++ Task-3/add_numbers.py
@@ -0,0 +1,10 @@
+def add_large_numbers(num1, num2):
+    return num1 + num2
+
+# Input: Two large numbers
+number1 = int(input("Input Number 1: "))
+number2 = int(input("Input Number 2: "))
+
+# Output: Sum of the numbers
+sum_of_numbers = add_large_numbers(number1, number2)
+print("Sum of Numbers:", sum_of_numbers)
